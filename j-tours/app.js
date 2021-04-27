379facb3c391bf3fe51e212231d4a1de8da2765f
Uvejs <uvejs.danjolli04@gmail.com>
2021-04-27 19:23:06 +0200

    POSTMAN & Node APP

---
+++ j-tours/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const fs = require('fs')
+
+
+
+const app = express()
+
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello from server side",
+        app: "jTours"
+    })
+})
+
+
+app.post("/", (req, res) => {
+    res.send("You can post to this URL")
+})
+
+
+const tours = JSON.parse(fs.readFileSync('${__dirname}/dev-data/data/tours-simple.js')) 
+
+
+app.listen(3000, () => {
+    console.log("Server is listening")
+})
+
